17cce803710e05ce8d7eac3e1d892434567094cc
Praveen Rao V P <praveenraovp01@gmail.com>
2021-05-01 16:45:22 +0530

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -5,8 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 #set db name
-DB_NAME = 'database.db'
 db=SQLAlchemy()
+DB_NAME = 'database.db'
 
 #create window
 def create_app():
@@ -19,12 +19,24 @@ def create_app():
      from .views import views
      from .auth import auth
      
+     app.register_blueprint(views, url_prefix='/')
+     app.register_blueprint(auth, url_prefix = '/')
+     
+     from .models import User,Note
      create_database(app)
+     
+     login_manager = LoginManager()
+     login_manager.login_view = 'auth.login'
+     login_manager.init_app(app)
+     
+     def load_user(id):
+          return User.query.get(int(id))
+     
      return app 
 
 #create database
 def create_database(app):
      if not path.exists('website/'+DB_NAME):
           db.create_all(app=app)
-          
+          print("Created the database!")
           
